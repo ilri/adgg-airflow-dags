80306457640db6d1bfbd454c1b6e60241c451620
GKosgei <mr.gkosgei@gmail.com>
2023-11-17 15:46:36 +0300

    [bug] script to check if animal is the sire of its sire

--- utilities/scripts/pedigree/pedigree_check_grandsire.sql
+++ utilities/scripts/pedigree/pedigree_check_grandsire.sql
@@ -2,7 +2,7 @@ SET @uuid = '{{ params.uuid }}';
 
 -- Progeny Is Its Own Grand Sire
 UPDATE reports.staging_pedigree_data a
-JOIN (SELECT id FROM reports.staging_pedigree_data WHERE uuid = @uuid AND id = grand_sire_id) b
+JOIN (SELECT id FROM reports.staging_pedigree_data WHERE uuid = @uuid AND animal_id = grand_sire_id) b
 ON a.id = b.id SET a.status = 0, a.error = CONCAT(ifnull(a.error,''),' | ','Progeny Is Its Own Grand Sire')
 WHERE a.uuid = @uuid;
 
