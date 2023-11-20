3af8deb8a60b822a33573fff5aada987a399d0b3
GKosgei <mr.gkosgei@gmail.com>
2023-11-20 10:48:43 +0300

    pedigree dag: use tag id as basis for checking duplicates

--- utilities/scripts/pedigree/pedigree_check_duplicates.sql
+++ utilities/scripts/pedigree/pedigree_check_duplicates.sql
@@ -1,10 +1,20 @@
 SET @uuid = '{{ params.uuid }}';
 
 UPDATE reports.staging_pedigree_data a 
-JOIN (SELECT animal_id,farm_id 
-FROM reports.staging_pedigree_data WHERE uuid = @uuid GROUP BY animal_id,farm_id HAVING COUNT(animal_id)>1) b 
-ON a.animal_id = b.animal_id SET a.status = 0, a.error = CONCAT(ifnull(a.error,''),' | ','Duplicate Record')
+JOIN (SELECT tag_id
+FROM reports.staging_pedigree_data WHERE uuid = @uuid GROUP BY tag_id HAVING COUNT(tag_id)>1) b
+ON a.tag_id = b.tag_id SET a.status = 0, a.error = CONCAT(ifnull(a.error,''),' | ','Duplicate Record')
 WHERE a.uuid =   @uuid;
+
+
+--UPDATE reports.staging_pedigree_data a
+--JOIN (SELECT animal_id,farm_id
+--FROM reports.staging_pedigree_data WHERE uuid = @uuid GROUP BY animal_id,farm_id HAVING COUNT(animal_id)>1) b
+--ON a.animal_id = b.animal_id SET a.status = 0, a.error = CONCAT(ifnull(a.error,''),' | ','Duplicate Record')
+--WHERE a.uuid =   @uuid;
+
+
+
 
 
 
