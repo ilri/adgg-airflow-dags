71fabbdef29f82581d2e95fbd6fc8b46c39f1bc5
GKosgei <mr.gkosgei@gmail.com>
2023-11-22 19:16:07 +0300

    bisexual check bugs

--- utilities/scripts/pedigree/pedigree_check_bisexuals.sql
+++ utilities/scripts/pedigree/pedigree_check_bisexuals.sql
@@ -3,9 +3,12 @@ SET @uuid = '{{ params.uuid }}';
 -- Bisexual Check
 UPDATE reports.staging_pedigree_data x
 JOIN (select a.sire_id animal_id from reports.staging_pedigree_data a
-join reports.staging_pedigree_data b on a.sire_id = b.dam_id and a.uuid = @uuid and b.uuid = @uuid) y
-ON x.animal_id = y.animal_id SET x.status = 0, x.error = CONCAT(ifnull(x.error,''),' | ','Bisexual(Used A s Sire & Dam)')
+join adgg_uat.core_animal b on a.sire_id = b.dam_id and a.country_id = b.country_id and a.uuid = @uuid and b.uuid = @uuid) y
+ON x.animal_id = y.animal_id SET x.status = 0, x.error = CONCAT(ifnull(x.error,''),' | ','Bisexual(Sire used as Dam)')
 WHERE x.uuid = @uuid;
+
+
+
 
 
 
