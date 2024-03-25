3c9559a6c3d51225ec7366fc381eed17fdde20eb
Jennifer Volk <jvolk@ed.ac.uk>
2024-03-25 17:32:11 +0000

    Updated procedure call template

--- utilities/templates/procedurecall_template.py
+++ utilities/templates/procedurecall_template.py
@@ -3,31 +3,32 @@ import os
 
 from datetime import datetime, timedelta
 from airflow.decorators import dag, task
-from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 
-mysql_conn='mysql_adgg_db_production'
-dagid='PROD_ADMIN_clean_scratchdb'
+dagid='DEV_set_animal_inactive' #create a unique ID which will appear in the DAG list
 scheduleinterval="0 2 * * 0"  # utc 
-sqlproc="call workdb.spm_ADMIN_cleanup_scratchdb"
-taskid = sqlproc[0,30]
-tag1 = 'Sample'
-tag2 = 'Another tag'
+
+tag1 = 'DQ'
+tag2 = 'core_animal'
+tag3 = 'scheduled update'
 now = datetime.now()
 
 ## Define any parameters you want to set at run time
 dag_params = {
-    'param1': 90,
-    'param2': 365
+    'database_name': 'adgg',
+    'max_animal_years_old': 20,
+    'last_event_years_old': 5   
 }
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2023, 7, 20),
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5)
 }
+
 @dag(
     dag_id=dagid,
     default_args=default_args,
@@ -35,20 +36,23 @@ default_args = {
     catchup=False,
     max_active_runs=1,  # Set the maximum number of active runs to 1
     params=dag_params,
-    tags=[tag1, tag2])
+    tags=[tag1, tag2,tag3]
 )
+
 
 def run_db_proc():
     @task(task_id="Start", provide_context=True)
     def start():
         return "start"
-    
-    sqlstmt="""{{sqlproc}}({{params.param1}}, {{params.param2}})"""
 
-    execproc= MySqlOperator(     
-        task_id=taskid,     
-        mysql_conn_id=mysql_conn,     
+    ## do procedure call and interpolate the parameters as set up in dag_params
+    sqlstmt="""call workdb.spm_set_animal_inactive('{{params.database_name}}', {{params.max_animal_years_old}}, {{params.last_event_years_old}})"""
+
+    execproc= SQLExecuteQueryOperator(     
+        task_id='Execute_proc',     
+        conn_id='mysql_adgg_db_dev',   #  OR   conn_id='mysql_adgg_db_production',    
         sql=sqlstmt,
+        autocommit=True,
         parameters=dag_params
         )
     
